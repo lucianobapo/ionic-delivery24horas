f76792230ae96d32eefae56c8b662e382ba0f43d
Luciano Porto <luciano.bapo@gmail.com>
2016-03-15 04:23:08 -0300

    ionic new structure

--- resources/js/app.js
+++ resources/js/app.js
@@ -78,7 +78,30 @@
         }
     ]);
 
+    appModule.config(function($httpProvider) {
+        $httpProvider.interceptors.push(function($rootScope) {
+            return {
+                request: function(config) {
+                    $rootScope.$broadcast('loading:show');
+                    return config;
+                },
+                response: function(response) {
+                    $rootScope.$broadcast('loading:hide');
+                    return response;
+                }
+            }
+        });
+    });
 
+    appModule.run(function($rootScope, $ionicLoading) {
+        $rootScope.$on('loading:show', function() {
+            $ionicLoading.show({template: "<p>Carregando...</p><ion-spinner></ion-spinner>"});
+        });
+
+        $rootScope.$on('loading:hide', function() {
+            $ionicLoading.hide();
+        });
+    });
 
     appModule.run([
         '$ionicPlatform',
