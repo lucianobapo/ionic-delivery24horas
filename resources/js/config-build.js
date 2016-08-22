0995cde066dc6353e2ed89cbb8ba5ad95dc6922f
Luciano Porto <luciano.bapo@gmail.com>
2016-08-22 05:58:40 -0300

    update to v0.1.17

--- resources/js/config-build.js
+++ resources/js/config-build.js
@@ -12,11 +12,11 @@
     var configModule = angular.module('App.Config');
 
     configModule.constant('AppConfig', {
-        debug: false,
+        debug: true,
         cordova: false,
-        facebookID: '1581785262035600',
+        facebookID: '1630647053816087',
         servicoCep: function(query){ return 'https://viacep.com.br/ws/'+query+'/json/'; },
-        apiEndpoint: 'http://api.delivery24horas.com/json',
+        apiEndpoint: 'http://api.localhost.com/json',
         imagesUrl: 'https://s3.amazonaws.com/delivery-images/thumbnails/',
         logoUrl: 'https://s3.amazonaws.com/delivery-images/logo/'
     });
