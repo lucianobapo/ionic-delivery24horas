a5ddc8bad051170364e172c613fdc72646443d2a
Luciano Porto <luciano.bapo@gmail.com>
2016-09-07 22:43:21 -0300

    update to v0.1.17

--- resources/js/config.js
+++ resources/js/config.js
@@ -19,7 +19,7 @@
         cordova: true,
         facebookID: '1581785262035600',
         servicoCep: function(query){ return 'https://viacep.com.br/ws/'+query+'/json/'; },
-        apiEndpoint: 'https://api.ilhanet.com/json',
+        apiEndpoint: 'https://api.ilhanet.com/api',
         imagesUrl: 'https://storage.googleapis.com/ilhanet-140808.appspot.com/thumbnails/',
         campanhasUrl: 'https://storage.googleapis.com/ilhanet-140808.appspot.com/campanhas/',
         logoUrl: 'https://storage.googleapis.com/ilhanet-140808.appspot.com/logo/'
@@ -34,7 +34,7 @@
         cordova: false,
         facebookID: '1581785262035600',
         servicoCep: function(query){ return 'https://viacep.com.br/ws/'+query+'/json/'; },
-        apiEndpoint: 'https://api.ilhanet.com/json',
+        apiEndpoint: 'https://api.ilhanet.com/api',
         imagesUrl: 'https://storage.googleapis.com/ilhanet-140808.appspot.com/thumbnails/',
         campanhasUrl: 'https://storage.googleapis.com/ilhanet-140808.appspot.com/campanhas/',
         logoUrl: 'https://storage.googleapis.com/ilhanet-140808.appspot.com/logo/'
@@ -49,7 +49,7 @@
         cordova: false,
         facebookID: '1630647053816087',
         servicoCep: function(query){ return 'https://viacep.com.br/ws/'+query+'/json/'; },
-        apiEndpoint: 'http://api.localhost.com/json',
+        apiEndpoint: 'http://api.localhost.com/api',
         imagesUrl: 'https://storage.googleapis.com/ilhanet-140808.appspot.com/thumbnails/',
         campanhasUrl: 'https://storage.googleapis.com/ilhanet-140808.appspot.com/campanhas/',
         logoUrl: 'https://storage.googleapis.com/ilhanet-140808.appspot.com/logo/'
@@ -64,7 +64,7 @@
         cordova: true,
         facebookID: '1630647053816087',
         servicoCep: function(query){ return 'https://viacep.com.br/ws/'+query+'/json/'; },
-        apiEndpoint: '/json',
+        apiEndpoint: '/api',
         imagesUrl: 'https://storage.googleapis.com/ilhanet-140808.appspot.com/thumbnails/',
         campanhasUrl: 'https://storage.googleapis.com/ilhanet-140808.appspot.com/campanhas/',
         logoUrl: 'https://storage.googleapis.com/ilhanet-140808.appspot.com/logo/'
--- www/js/bundles/app.bundle.js
+++ www/js/bundles/app.bundle.js
@@ -434,7 +434,7 @@
         cordova: false,
         facebookID: '1581785262035600',
         servicoCep: function(query){ return 'https://viacep.com.br/ws/'+query+'/json/'; },
-        apiEndpoint: 'https://api.ilhanet.com/json',
+        apiEndpoint: 'https://api.ilhanet.com/api',
         imagesUrl: 'https://storage.googleapis.com/ilhanet-140808.appspot.com/thumbnails/',
         campanhasUrl: 'https://storage.googleapis.com/ilhanet-140808.appspot.com/campanhas/',
         logoUrl: 'https://storage.googleapis.com/ilhanet-140808.appspot.com/logo/'
